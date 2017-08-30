fe074e4e016e7405e1239e5debb79d84400411d7
DhanyaHerath <dhanya@sussol.net>
2017-08-30 12:51:46 +1200

    Fix commit changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { UserList, UserEdit, UserCreate } from './users'
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://localhost:4000/api')}>
         <Resource name="companies" list={CompanyList} edit={CompanyEdit} create={CompanyCreate}/>
-        <Resource name="users" list={UserList} />
+        <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate}/>
     </Admin>
 );
 
