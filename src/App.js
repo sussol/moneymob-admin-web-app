57a8c2bb682048a886fc5ab5ad1b6f479b3b2b1f
DhanyaHerath <dhanya@Dhanyas-MacBook-Pro.local>
2017-08-30 12:58:58 +1200

    Add delete buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // file: src/App.js
 
 import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 
 import { CompanyList, CompanyEdit, CompanyCreate } from './companies';
 import { UserList, UserEdit, UserCreate } from './users'
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://localhost:4000/api')}>
-        <Resource name="companies" list={CompanyList} edit={CompanyEdit} create={CompanyCreate}/>
-        <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate}/>
+        <Resource name="companies" list={CompanyList} edit={CompanyEdit} create={CompanyCreate} remove={Delete}/>
+        <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete}/>
     </Admin>
 );
 
