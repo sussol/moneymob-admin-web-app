632c9ffeeb03d59b070a98852a6b694c43ba52d0
sinclairsystems <chrissinclairsystems@gmail.com>
2017-09-01 12:13:50 +1200

    Revert "Revert "Add login interface""

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// file: src/App.js
+ // file: src/App.js
 
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 
+import authClient from './authClient';
 import { CompanyList, CompanyEdit, CompanyCreate } from './companies';
 import { UserList, UserEdit, UserCreate } from './users'
 
 const App = () => (
-    <Admin restClient={jsonServerRestClient('http://localhost:4000/api')}>
+    <Admin authClient={authClient} restClient={jsonServerRestClient('http://localhost:4000/api')}>
         <Resource name="companies" list={CompanyList} edit={CompanyEdit} create={CompanyCreate} remove={Delete}/>
         <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete}/>
     </Admin>
