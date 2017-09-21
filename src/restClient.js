fe703179db8418717dc224edf387eeff8206c751
nmadruga <nicole@sussol.net>
2017-09-21 16:35:18 +1200

    Update yarn.lock with lastest

--- src/restClient.js
+++ src/restClient.js
@@ -1,2 +1,2 @@
 import { jsonServerRestClient } from 'admin-on-rest';
-export default jsonServerRestClient('http://localhost:4000/api');
+export default jsonServerRestClient('http://localhost:4000/api/v1');
